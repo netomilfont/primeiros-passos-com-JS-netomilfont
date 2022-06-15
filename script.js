c5850a1957993d8e359794d5e03b5f781ee45159
netomilfont <netomilfont@hotmail.com>
2022-06-15 13:27:28 -0300

    corrigindo valor

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ const feijao = 11.89
 const vinho = 70
 
 
-const precoRefrigerante = 12.99 * 3
+const precoRefrigerante = 11.99 * 3
 const precoMacarrao = 6.99 * 4 
 const precoErvilha = 6.99 * 3
 const precoArroz = 22.99 * 3
 const precoFeijao = 11.89 * 2
 const precoVinho = 70 * 3
 
-let total = parseInt(precoVinho + precoFeijao + precoArroz + precoErvilha + precoMacarrao + precoRefrigerante)
+let total = parseFloat(precoVinho + precoFeijao + precoArroz + precoErvilha + precoMacarrao + precoRefrigerante)
+
 
 let metade = total/2
 
@@ -39,22 +40,22 @@ R$${voce} reais e o meu amigo irá pagar R$${amigo} reais.`)
 let voceVaiPagar = 0
 let amigoVaiPagar = 0
 
-let valorRefrigerante = parseInt(prompt("Digite o preço do refrigerante"))
-let quantidadeRefrigerante = parseInt(prompt("Digite a quantidade de refrigerante"))
+let valorRefrigerante = parseFloat(prompt("Digite o preço do refrigerante"))
+let quantidadeRefrigerante = parseFloat(prompt("Digite a quantidade de refrigerante"))
 
-let valorMacarrao = parseInt(prompt("Digite o preço do macarrão"))
+let valorMacarrao = parseFloat(prompt("Digite o preço do macarrão"))
 let quantidadeMacarrao = parseInt(prompt("Digite a quantidade de macarrão"))
 
-let valorErvilha = parseInt(prompt("Digite o preço da ervilha"))
+let valorErvilha = parseFloat(prompt("Digite o preço da ervilha"))
 let quantidadeErvilha = parseInt(prompt("Digite a quantidade de ervilha"))
 
-let valorArroz = parseInt(prompt("Digite o preço do arroz"))
+let valorArroz = parseFloat(prompt("Digite o preço do arroz"))
 let quantidadeArroz = parseInt(prompt("Digite a quantidade de arroz"))
 
-let valorFeijao = parseInt(prompt("Digite o preço do feijão"))
+let valorFeijao = parseFloat(prompt("Digite o preço do feijão"))
 let quantidadeFeijao = parseInt(prompt("Digite a quantidade de feijão"))
 
-let valorVinho = parseInt(prompt("Digite o preço do vinho"))
+let valorVinho = parseFloat(prompt("Digite o preço do vinho"))
 let quantidadeVinho = parseInt(prompt("Digite a quantidade de vinho"))
 
 let totalRefrigerante = valorRefrigerante * quantidadeRefrigerante
@@ -64,7 +65,7 @@ let totalArroz = valorArroz * quantidadeArroz
 let totalFeijao = valorFeijao * quantidadeFeijao
 let totalVinho = valorVinho * quantidadeVinho
 
-let totalDasCompras = totalRefrigerante + totalMacarrao + totalErvilha + totalArroz + totalFeijao + totalMacarrao
+let totalDasCompras = totalRefrigerante + totalMacarrao + totalErvilha + totalArroz + totalFeijao + totalVinho
 
 let metadeDasCompras = totalDasCompras/2
 
